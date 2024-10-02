4ac2fdaa8622eed04e6bcfd55257cb6b3f59b5d4
T0bbuT <shanghaialicegengakudan1@gmail.com>
2024-10-02 15:34:50 +0900

    ・一度提出用データを出力する所まで行った ・元のcsvにあるIdを別の変数に格納しておくようにした

--- 再分析.py
+++ 再分析.py
@@ -3,7 +3,7 @@
 
 # # 1. EDA
 
-# In[2]:
+# In[6]:
 
 
 import os
@@ -19,18 +19,21 @@ from scipy import special
 pd.set_option("display.max_columns", 100)
 pd.set_option("display.max_rows", 500)
 
-df_train = pd.read_csv("train_test_submission/train.csv").drop("Id", axis=1)
-df_test = pd.read_csv("train_test_submission/test.csv").drop("Id", axis=1)
+df_train = pd.read_csv("train_test_submission/train.csv")
+df_train_Id = df_train["Id"]
+df_train = df_train.drop("Id", axis=1)
+
+df_test = pd.read_csv("train_test_submission/test.csv")
+df_test_Id = df_test["Id"]
+df_test = df_test.drop("Id", axis=1)
 
 print(f"df_train.shape: {df_train.shape}")
 display(df_train.head(5))
-# print(df_train.info())
 print(f"df_train.shape: {df_test.shape}")
 display(df_test.head(5))
-# print(df_test.info())
 
 
-# In[3]:
+# In[7]:
 
 
 print("-" * 10, "df_train.info()", "-" * 10)
@@ -40,7 +43,7 @@ print("-" * 10, "df_test.info()", "-" * 10)
 print(df_test.info())
 
 
-# In[4]:
+# In[8]:
 
 
 # # ydata_profilingを使う場合。時間かかるので注意
@@ -55,7 +58,7 @@ print(df_test.info())
 # # profile.to_file("ydata_profiling/kaggle_houseprices.html")
 
 
-# In[5]:
+# In[9]:
 
 
 print("-" * 10, 'df_train["SalePrice"].describe()', "-" * 10)
@@ -68,7 +71,7 @@ plt.suptitle("SalePriceの分布")
 plt.show()
 
 
-# In[6]:
+# In[10]:
 
 
 corr_matrix = df_train.corr(numeric_only=True)
@@ -84,7 +87,7 @@ plt.suptitle("訓練データの相関係数(絶対値)行列")
 plt.show()
 
 
-# In[7]:
+# In[11]:
 
 
 threshold = 0.6
@@ -114,7 +117,7 @@ plt.show()
 
 # # 2. 前処理(とりあえずlightGBMで回すために)
 
-# In[8]:
+# In[12]:
 
 
 # lightGBMに突っ込むためには数値型(またはbool型)である必要があるので、object型のデータをlabel encodingで処理する
@@ -126,17 +129,21 @@ from sklearn.model_selection import KFold
 from lightgbm import LGBMRegressor
 from sklearn.metrics import root_mean_squared_error as rmse
 
+# object型のデータが入っている列を抽出
 object_columns = df_train.select_dtypes(include="object").columns
 df_train_pre_encoding = df_train.copy()
+df_test_pre_encoding = df_test.copy()
 
+# ラベルエンコーディング
 oe = OrdinalEncoder()
 df_train[object_columns] = oe.fit_transform(df_train[object_columns])
+df_test[object_columns] = oe.fit_transform(df_test[object_columns])
 
 # display(df_train_pre_encoding.head(10))
 # display(df_train.head(10))
 
 
-# In[9]:
+# In[13]:
 
 
 X = df_train.drop(["SalePrice"], axis=1)
@@ -163,6 +170,17 @@ print(f"\n\nThe score is {np.mean(scores)}.")
 # 次にやること：max_depthのチューニングってどうやればいいだろう？てか一回さっさと提出してみないか？
 # メモ：[LightGBM] [Warning] No further splits with positive gain, best gain: -infについて
 # これは「決定木の作成中、これ以上分岐を作っても予測誤差が下がらなかったのでこれ以上分岐をさせなかった」ことを意味するらしい
+
+
+# In[14]:
+
+
+# 一度このまま提出用のデータを出力してしまおう
+model = LGBMRegressor(max_depth=-1)
+model.fit(X, y)
+sub_pred = model.predict(df_test)
+submission = pd.DataFrame({"Id": df_test_Id, "SalePrice": sub_pred})
+submission.to_csv("train_test_submission\submission.csv", index=False)
 
 
 # In[ ]:
