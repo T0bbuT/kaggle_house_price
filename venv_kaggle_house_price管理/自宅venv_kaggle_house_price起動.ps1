efac5419ccb5f37da56b2c4fc97d49f6926e1c08
tamag0123 <shanghaialicegengakudan1@gmail.com>
2026-02-23 11:15:38 +0900

    要件ファイルの追加と未使用のスクリプトの削除
    - 依存関係管理用の requirements.in と requirements.txt を作成しました。
    - nbconvert_runner_pipx.ps1 スクリプトは不要になったため削除しました。
    - README.md と仮想環境の起動スクリプトは古くなったため削除しました。

--- venv_kaggle_house_price管理/自宅venv_kaggle_house_price起動.ps1
+++
@@ -1,7 +0,0 @@
-# 起動.ps1
-
-# このスクリプトのある場所に移動
-Set-Location -Path $PSScriptRoot
-
-# 実行したいスクリプトを呼び出し
-pwsh -NoExit -ExecutionPolicy Bypass -File "C:\Users\shang\venvs\venv_kaggle_house_price\Scripts\Activate.ps1"
